743d6e0119df83f1eeb6acd4fe940f2746e399b8
Ankitjigna <ankitjigna01@gmail.com>
2024-09-19 10:12:55 +0530

    adding delete and edit part

--- index.js
+++ index.js
@@ -2,35 +2,42 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+const { v4:  uuidv4 } = require('uuid');
+const methodOverride = require('method-override');
+
 app.use(express.urlencoded({extends:true}));
-// app.use()
+app.use(methodOverride("_method"));
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 
-app.set(express.static(path.join(__dirname,"views")));
+app.use(express.static(path.join(__dirname,"views")));
 
 let posts = [
     {
+        id: uuidv4(),
         username:"shukla",
         content: "preparing for gate"
     },
 
     {
+        id: uuidv4(),
         username:"dwivedi",
         content: "preparing for placement"
     },
 
     {
+        id: uuidv4(),
         username:"Sunny",
         content:" just wanna take degree"
     },
 
     {
-        username:"bhashkar",
+        id: uuidv4(),
+        username: "bhashkar",
         content: "preparing off campus placement"
-    }
-]
+    },
+];
 
 app.listen(port,()=>{
     console.log(`listening on port ${port}`);
@@ -46,9 +53,37 @@ app.get("/posts/new",(req,res)=>{
 })
 
 app.post("/posts",(req,res)=>{
-    // let {username,content}=req.body;
-    // posts.push={username,content};
     console.log(req.body);
-    res.send("post is working")
-    // res.render("index.ejs",{posts)});
+    let { username, content } = req.body;
+    let id = uuidv4();
+    posts.push({ id, username, content });
+    res.redirect("/posts");
+})
+
+app.get("/posts/:id",(req,res)=>{
+    let { id } = req.params;
+    let post= posts.find((p)=>id === p.id);
+    res.render("show.ejs",{ post});
+})
+
+app.patch("/posts/:id",(req,res)=>{
+    let { id } = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p)=> id ===p.id);
+    post.content = newContent;
+    // res.send("patch is working");
+    res.redirect("/posts");
+})
+
+app.get("/posts/:id/edit",(req,res)=>{
+    let { id } = req.params;
+    let post = posts.find((p)=> id ===p.id);
+    res.render("edit.ejs",{ posts });
+});
+
+app.delete("/posts/:id",(req,res)=>{
+    let { id } = req.params;
+    posts = posts.filter((p)=> id !==p.id);
+    // res.send("deleted");
+    res.redirect("/posts");
 })
